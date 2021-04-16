e7e61364981da3e628f1037039268155629a2159
Fleris <fleris.ramirez@socobox.co>
2021-04-16 08:39:48 -0500

    Funcionalidad para eliminar una película.

--- src/content/actions/favorite.js
+++ src/content/actions/favorite.js
@@ -2,10 +2,25 @@ import * as actionTypes from '../constants/actions';
 
 
 
-export const addFavorite=(data)=>(dispatch)=>{
-    
+export const addFavorite=(data)=>(dispatch, getState)=>{
+   
+    let favorite=[...getState().movieFavorites.favorite];
+    for (const iterator of favorite) {
+        if(iterator.id==data.id){
+          return
+        }
+    }
     dispatch({
         type:actionTypes.FAVORITE_MOVIE,
         payload:data
     })
+}
+
+export const deleteMovie = (id)=>(dispatch)=>{
+   
+   dispatch({
+       type:actionTypes.DELETE_FAVORITE,
+       payload:id
+   })
+    
 }
