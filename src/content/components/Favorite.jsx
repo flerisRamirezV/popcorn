9c4eae88bdef10b68d3c3b030fe0f3d262e96f87
Fleris <fleris.ramirez@socobox.co>
2021-04-16 08:44:43 -0500

    Agregada funcionalidad para eliminar una película de favoritos.

--- src/content/components/Favorite.jsx
+++ src/content/components/Favorite.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
-export const Favorite=(props)=> {
-    return (
-        <div className="movie__fav">
-           <p>{props.original_title.slice(0,9)}</p>
-        </div>  
-        )
-}
+export const Favorite = (props) => {
+  return (
+    <div className="container__image">
+      <i
+        className="fas fa-times-circle icon__popular--movie"
+        onClick={props.handleDelete}
+      ></i>
+
+      <img
+        src={`https://image.tmdb.org/t/p/w500${props.backdrop_path}`}
+        className="image"
+      />
+    </div>
+  );
+};
