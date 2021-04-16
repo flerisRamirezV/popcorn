774ece9e2bc3f3f8069ea52a6dd07a5a297c7182
Fleris <fleris.ramirez@socobox.co>
2021-04-16 08:45:17 -0500

    Agregado reducer para eliminar una película de favoritos.

--- src/content/reducer/MovieFavoriteReducer.jsx
+++ src/content/reducer/MovieFavoriteReducer.jsx
@@ -1,19 +1,20 @@
 import * as actionType from "../constants/actions";
 
 const initialState = {
- favorite:[],
+  favorite: [],
+  noFavorite: [],
 };
 
-export const  movieFavorites = (state = initialState, action) => {
+export const movieFavorites = (state = initialState, action) => {
   switch (action.type) {
     case actionType.FAVORITE_MOVIE:
-      
-      return { ...state, favorite:[...state.favorite,action.payload], };
-    
+      return { ...state, favorite: [...state.favorite, action.payload] };
+    case actionType.DELETE_FAVORITE:
+      return {
+        ...state,
+        favorite: state.favorite.filter((fav) => fav.id !== action.payload),
+      };
     default:
       return state;
   }
 };
-
-
-
