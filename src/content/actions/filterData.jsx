81db403b7d691ee6e3d079993e2f2ed76a694823
Fleris <fleris.ramirez@socobox.co>
2021-04-16 08:40:58 -0500

    Agregada imagen por defecto.

--- src/content/actions/filterData.jsx
+++ src/content/actions/filterData.jsx
@@ -1,16 +1,14 @@
-import * as actionTypes from '../constants/actions';
-import {fixedEncodeURIComponent, sendHttpRequest} from '../helpers/request'
+import * as actionTypes from "../constants/actions";
+import { fixedEncodeURIComponent, sendHttpRequest } from "../helpers/request";
 
+export const filterData = (name) => async (dispatch) => {
+  let url = `${actionTypes.URL_SEARCH_MOVIE}=${fixedEncodeURIComponent(
+    name
+  )}&api_key=${actionTypes.API_KEY}`;
+  const responseData = await sendHttpRequest(url);
 
-export const filterData=(name)=>async(dispatch)=>{
-    let url=
-     `${actionTypes.URL_SEARCH_MOVIE}=${fixedEncodeURIComponent(name)}&api_key=${actionTypes.API_KEY}`
-    const responseData= await sendHttpRequest(url);
-
-    console.log("DATOS",responseData, );
-    dispatch({
-        type: actionTypes.FILTER_MOVIE,
-        payload: {data:responseData.results, name:name}
-      });
-
-}
+  dispatch({
+    type: actionTypes.FILTER_MOVIE,
+    payload: { data: responseData.results, name: name },
+  });
+};
