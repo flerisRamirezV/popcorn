bcadf581219eb25fbf58e873200070749d1e57f6
Fleris <fleris.ramirez@socobox.co>
2021-04-20 14:32:10 -0500

    Agregado watcher para guardar una película.

--- src/content/sagas/rootSaga.js
+++ src/content/sagas/rootSaga.js
@@ -1,12 +1,13 @@
 import {all} from 'redux-saga/effects'
 import {moviesSagasWatcher, moviesRatedSagasWatcher} from './movies/list';
 import {searchMoviesWatcher} from './movies/filterMovies'
-
+import {saveMovieWatcher} from '../sagas/movies/favorite'
 export default function* rootSaga(){
    
     yield all([
         moviesSagasWatcher(),
         moviesRatedSagasWatcher(),
-        searchMoviesWatcher()
+        searchMoviesWatcher(),
+        saveMovieWatcher(),
     ])
 }
