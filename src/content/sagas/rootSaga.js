52902ce50924db259ccc60e450d3b903b3674baa
Fleris <fleris.ramirez@socobox.co>
2021-04-20 12:57:30 -0500

    Agregado watchers.

---
+++ src/content/sagas/rootSaga.js
@@ -0,0 +1,12 @@
+import {all} from 'redux-saga/effects'
+import {moviesSagasWatcher, moviesRatedSagasWatcher} from './movies/list';
+import {searchMoviesWatcher} from './movies/filterMovies'
+
+export default function* rootSaga(){
+   
+    yield all([
+        moviesSagasWatcher(),
+        moviesRatedSagasWatcher(),
+        // searchMoviesWatcher()
+    ])
+}
