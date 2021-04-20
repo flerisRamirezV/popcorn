f6271ed10027512069b8005f51b3dddbb1c7bed6
Fleris <fleris.ramirez@socobox.co>
2021-04-20 10:30:27 -0500

    Agregado Middleware de redux saga.

--- src/store.js
+++ src/store.js
@@ -3,7 +3,10 @@ import thunk from 'redux-thunk';
 import rootReducer from './content/reducer/index';
 import {save, load} from 'redux-localstorage-simple';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './content/sagas/rootSaga';
 
+const sagaMiddleware = createSagaMiddleware();
 const initialData= save(
     {
       states:["movieFavorites"
@@ -14,7 +17,7 @@ const initialData= save(
   
   const createStoreWithMiddleware 
   = 
-  composeWithDevTools(applyMiddleware(thunk,initialData),)
+  composeWithDevTools(applyMiddleware(thunk,sagaMiddleware,initialData),)
       
   (createStore)
   
@@ -26,5 +29,5 @@ const initialData= save(
            ]     
        }) 
    )  
-  
+   sagaMiddleware.run(rootSaga)
   export default store;
