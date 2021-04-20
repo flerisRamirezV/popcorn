9f93fc6c2d241d661a1f25e3484c5009e52a4f05
Fleris <fleris.ramirez@socobox.co>
2021-04-20 12:56:43 -0500

    Agregada funcionalidad para obtener los datos de la api.

--- src/content/actions/getMovies.jsx
+++ src/content/actions/getMovies.jsx
@@ -1,27 +1,40 @@
-import {sendHttpRequest} from '../helpers/request';
-import * as actionTypes from '../constants/actions';
+import { sendHttpRequest } from "../helpers/request";
+import * as actionTypes from "../constants/actions";
 
-export const getMovies=()=>async (dispatch)=>{
-    try {
-        const responseData = await sendHttpRequest(actionTypes.URL);
-        console.log(responseData);
-        dispatch({
-            type: actionTypes.GET_MOVIES,
-            payload: responseData.results
-        })
-    } catch (error) {
-        console.log("error",error);
-    }
-}
-export const getMoviesRated=()=>async (dispatch)=>{
-    try {
-        const responseData = await sendHttpRequest(actionTypes.URL_API_RATED);
-       
-        dispatch({
-            type: actionTypes.GET_MOVIES_RATED,
-            payload: responseData.results
-        })
-    } catch (error) {
-        console.log("error",error);
-    }
-}
+export const getMovies = () => async (dispatch) => {
+  try {
+    const responseData = await sendHttpRequest(actionTypes.URL);
+    console.log(responseData);
+    dispatch({
+      type: actionTypes.GET_MOVIES,
+      payload: responseData.results,
+    });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+export const getMoviesRated = () => async (dispatch) => {
+  try {
+    const responseData = await sendHttpRequest(actionTypes.URL_API_RATED);
+
+    dispatch({
+      type: actionTypes.GET_MOVIES_RATED,
+      payload: responseData.results,
+    });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export const getMovie = () => {
+  return {
+    type: actionTypes.GET_MOVIES,
+  };
+};
+
+export const getMovieRated = (payload) => {
+  return {
+    type: actionTypes.GET_MOVIES_RATED,
+    payload,
+  };
+};
