331b33dfef418b60a5fc393c8c05ee6cd107ce55
Fleris <fleris.ramirez@socobox.co>
2021-04-21 08:20:40 -0500

    Creada funcionalidad delay.

--- src/content/helpers/events.js
+++ src/content/helpers/events.js
@@ -1,8 +1,10 @@
-import * as actionTypes from '../constants/actions';
+import * as actionTypes from "../constants/actions";
 
+export const toogleModal = () => (dispatch) => {
+  dispatch({
+    type: actionTypes.TOGGLE_MODAL,
+  });
+};
 
-export const toogleModal=()=>dispatch=>{
-    dispatch({
-        type:actionTypes.TOGGLE_MODAL
-    })
-}
+export const delay = (time) =>
+  new Promise((resolve) => setTimeout(resolve, time));
