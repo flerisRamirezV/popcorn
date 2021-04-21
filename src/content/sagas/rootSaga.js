6dfeb93ca8b15848401152de5a67b835ea947bee
Fleris <fleris.ramirez@socobox.co>
2021-04-21 08:27:01 -0500

    Cambiado watcher para películas favoritas.

--- src/content/sagas/rootSaga.js
+++ src/content/sagas/rootSaga.js
@@ -1,13 +1,14 @@
 import {all} from 'redux-saga/effects'
 import {moviesSagasWatcher, moviesRatedSagasWatcher} from './movies/list';
 import {searchMoviesWatcher} from './movies/filterMovies'
-import {saveMovieWatcher} from '../sagas/movies/favorite'
+import {movieFavoriteWatcher} from '../sagas/movies/favorite'
 export default function* rootSaga(){
    
     yield all([
         moviesSagasWatcher(),
         moviesRatedSagasWatcher(),
         searchMoviesWatcher(),
-        saveMovieWatcher(),
+        movieFavoriteWatcher(),
+        
     ])
 }
