dd7ef8f353f59d48cbb970b2d3ac3cc4c41ed839
Fleris <fleris.ramirez@socobox.co>
2021-04-21 08:32:23 -0500

    Ui para el loading.

---
+++ src/content/ui/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/loading.css";
+export const Loading = () => {
+  return (
+    <div className="loading__content">
+      <div className="loading"></div>
+       <div className="loading"></div>
+      <div className="loading"></div> 
+    </div>
+  );
+};
