a6d18afd79081cc4d28c2df9d3cabf7a5aa11148
Fleris <fleris.ramirez@socobox.co>
2021-04-21 10:11:22 -0500

    Agregado redux saga a la funcionalidad de añadir una película a la lista de favoritos.

--- src/content/sagas/movies/favorite.js
+++ src/content/sagas/movies/favorite.js
@@ -1,7 +1,12 @@
-import { put, takeLatest, all } from "redux-saga/effects";
+import { put, takeLatest, all, select } from "redux-saga/effects";
 
 import * as actionTypes from "../../constants/actions";
-import { saveMovieFavorite,deleteMovie } from "../../actions/favorite";
+import {
+  saveMovieFavorite,
+  deleteMovie,
+  addMovieFavorite,
+  arrayFavorite,
+} from "../../actions/favorite";
 
 function* saveFavoriteMovie(data) {
   try {
@@ -11,21 +16,31 @@ function* saveFavoriteMovie(data) {
   }
 }
 
-function* deleteMovieFavorite(data){
-   
-    try {
-        yield put(deleteMovie(data.id))
-    } catch (error) {
-        console.log(error)
+function* deleteMovieFavorite(data) {
+  try {
+    yield put(deleteMovie(data.id));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function* addMoviesFavorite(movie) {
+  try {
+    const favorite = (movies) => movies.movieFavorites.favorite;
+    const items = yield select(favorite);
+    const res = yield arrayFavorite(movie, items);
+    if (!res) {
+      yield put(addMovieFavorite(movie.data));
     }
+  } catch (error) {
+    console.log("ERROR", error)
+  }
 }
 
 export function* movieFavoriteWatcher() {
-   
-    yield all([
-        yield takeLatest(actionTypes.SAVE_FAVORITE_MOVIE, saveFavoriteMovie),
-        yield takeLatest(actionTypes.DELETE_MOVIE, deleteMovieFavorite)
-    ])
- 
+  yield all([
+    yield takeLatest(actionTypes.SAVE_FAVORITE_MOVIE, saveFavoriteMovie),
+    yield takeLatest(actionTypes.DELETE_MOVIE, deleteMovieFavorite),
+    yield takeLatest(actionTypes.ADD_MOVIE_FAVORITE, addMoviesFavorite),
+  ]);
 }
-
