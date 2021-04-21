f7a96bcdee6b26e73dde676fecdd48fef0c327e8
Fleris <fleris.ramirez@socobox.co>
2021-04-21 08:25:26 -0500

    Agregada funcionalidad para eliminar una película.

--- src/content/components/myFavoriteMovie.jsx
+++ src/content/components/myFavoriteMovie.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useSelector, useDispatch} from "react-redux";
 import "../styles/favorite.css";
 import { Favorite } from "./Favorite";
-import {deleteMovie} from '../actions/favorite'
+import {deleteMovieFavorite } from '../actions/favorite';
 export const MyFavoriteMovie = () => {
   const favorite = useSelector((state) => state.movieFavorites.favorite);
   const dispatch = useDispatch();
 
   const handleDelete=(id)=>{
-    dispatch(deleteMovie(id))
+   
+    dispatch(deleteMovieFavorite(id))
   }
   return (
     <div className="favorite">
